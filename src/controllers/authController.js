e1bbbc10adc9ba344faeeb716036fef6e29e91a6
littleFootDev <thelittlefootdev@gmail.com>
2021-10-11 16:44:08 +0200

    add feature apiFeatures, and appError. And finish AuthCOntroller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,8 @@
 import crypto from 'crypto';
+import { promisify } from 'util';
 import jwt from 'jsonwebtoken';
 import User from '../models/user';
-
+import AppError from '../utils/appError';
 
 const signToken = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET, {
@@ -11,15 +12,15 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
-    const cookieOptions = { 
-        expires : new Date(
+    
+    res.cookie('jwt', token, { 
+        expiresIn: new Date(
             Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
         ),
-        httpOnly : true,
-    };
-    if(process.env.MODE_ENV === 'production') cookieOptions.secure = true;
+        httpOnly: true,
+        secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
+    });
 
-    res.cookie("jwt", token, cookieOptions);
 
     res.status(statusCode).json({
         status : "success",
@@ -31,32 +32,38 @@ const createSendToken = (user, statusCode, res) => {
 };
 
 const signUp = async (req, res, next) => {
+   try {
     const newUser = await User.create(req.body);
 
     const url = `${req.protocol}://${req.get("host")}/me`;
 
     createSendToken(newUser, 201, res);
+   } catch (err) {
+    console.log(`Erreur dans la création du compte :  ${err.message}`);
+   }
 };
 
 const login = async (req, res, next) => {
-    const {email, password} = req.body;
+    try {
+        const {email, password} = req.body;
 
-    if(!email || !password) {
-        console.log(`Entrer un email et un mot de passe ${err.message}`);
-        return next(err);
-    };
+        if(!email || !password) {
+            return next(new AppError('Please provide email and password!', 400));
+        };
 
-    const user = await User.findOne({email}).select("+password");
+        const user = await User.findOne({email}).select("+password");
 
-    if(!user || !(await user.correctPassword(password, user.password))) {
-        console.log(`Email ou mot de passe invalide ${err.message}`)
-        return next(err);
-    };
+        if(!user || !(await user.correctPassword(password, user.password))) {
+            return next(new AppError('Incorrect email or password', 401));
+        };
 
     createSendToken(user, 200, res);
+    } catch (err) {
+        console.log(`Erreur lors de la connection au compte:  ${err.message}`);
+    }
 };
 
-const logOut = async (req, res, next) => {
+const logOut =  (req, res, next) => {
     res.cookie("jwt", "loggedout", {
         expires: new Date(Date.now() + 10 * 1000),
         httpOnly : true,
@@ -66,32 +73,78 @@ const logOut = async (req, res, next) => {
 };
 
 const protect = async (req, res, next) => {
-    let token;
+   try {
+        let token;
 
-    if(
-        req.headers.authorization &&
-        req.headers.authorization.startsWith("Bearer")
-    ) {
-        token = req.headers.authorization.split(" ")[1];
-    } else if (req.cookie.jwt) {
-        token = req.token.jwt;
+        if(
+            req.headers.authorization &&
+            req.headers.authorization.startsWith("Bearer")
+        ) {
+            token = req.headers.authorization.split(" ")[1];
+        } else if (req.cookie.jwt) {
+            token = req.token.jwt;
+        };
+
+        if(!token) {
+            return next(
+                new AppError("Vous n'étes pas connecté. Veuillez vous connecter pour avoir acces au catalogue", 401)
+              );
+        };
+
+        const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+        const currentUser = await User.findById(decoded.id);
+        if(!currentUser) {
+            return next(
+                new AppError(
+                  "L'utilisateur qui ésaille de ce connecté existe pas.",
+                  401
+                )
+              );
+        };
+
+        req.user = currentUser;
+        res.locals.user = currentUser
+        next();
+   } catch (err) {
+    console.log(`L'utilisateur n'a pas l'autorisation d'accedé a ces pages:  ${err.message}`);
+   }
+};
+
+const isLoggedIn = async (req, res, next) => {
+    try {
+        if(req.cookie.jwt) {
+            try {
+                const decoded = await promisify(jwt.verify)(
+                    req.cookie.jwt,
+                    process.env.JWT_SECRET
+                );
+
+                const currentUser = await User.findById(decoded.id);
+                if(!currentUser) {
+                    return next();
+                };
+
+                res.locals.user = currentUser;
+                return next();
+            } catch (err) {
+                return next(err);
+            }
+        };
+        next();
+    } catch (err) {
+        console.log(`L'utilisateur n'est pas connecté:  ${err.message}`);
+    }
+};
+
+const restricTo = (...role) => {
+    return (req, res, next) => {
+        if(!role.includes(req.user.role)) {
+            return next(
+                new AppError("Vous n'avez pas la permission pour cette action", 403)
+              );
+        };
     };
+};
 
-    if(!token) {
-        console.log(`Vous n'étes pas connecté! Please connecté vous pour avoir accés au information ${err.message}` );
-        return next(err);
-    };
-
-    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-
-    const currentUser = await User.findById(decoded.id);
-    if(!currentUser) {
-        console.log(`l'utilisateur avec ce token n'existe pas ${err.message}`);
-        return next(err);
-    };
-
-    req.user = currentUser;
-    res.locals.user = currentUser
-    next();
-}
-export {signUp, login, logOut, protect};
+export {signUp, login, logOut, protect, isLoggedIn, restricTo};
