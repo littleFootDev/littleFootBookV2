132ef018877a664d8692ed260ef13e3d6a58fed3
littleFootDev <thelittlefootdev@gmail.com>
2021-10-18 07:35:59 +0200

    Add borrow models

---
+++ src/models/borrow.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+import User from '../models/user';
+import Book from '../models/book';
+
+const borrowSchema = new mongoose.Schema({
+    user: {
+        type : mongoose.Schema.ObjectId,
+        ref : 'User', 
+        required : [true, 'Un emprunt doit avoir un utilisateur.']
+    },
+    book: {
+        type : mongoose.Schema.ObjectId,
+        ref : 'Book', 
+        required : [true, 'Un emprunt doit avoir un livre']
+    },
+    dateOfBorrow: {
+        type : Date,
+        default : Date.now()
+    },
+    dateOfreturn : Date,
+    borrow: {
+        type : Boolean,
+        default : false
+    }
+});
+
+const Borrow = mongoose.model('Borrow', borrowSchema);
+
+export default Borrow;
